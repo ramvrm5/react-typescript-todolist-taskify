dfabbaea08c7f856be9876eadf74c6005f227b23
ram verma <42866242+ramvrm5@users.noreply.github.com>
2023-03-22 15:04:15 +0530

    changes in file Todo

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from "react";
+
 import { useRef } from "react";
+
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+
 import { MdDone } from "react-icons/md";
+
 import { Todo } from "../models/models";
+
 import { Draggable } from "react-beautiful-dnd";
 
 const SingleTodo: React.FC<{
